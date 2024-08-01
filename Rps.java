7c9ddd0ee312e77aa8a27d23b4ec834d338919fb
piyush-jethwa <piyushjethwa727@gmail.com>
2024-08-01 23:11:33 +0530

    Task2-Rock Paper Scissor  game

--- Rps.java
+++ Rps.java
@@ -1,7 +1,7 @@
 import java.util.Random;
 import java.util.Scanner;
 
-    public class Rps  {
+    public class RPS  {
         public static void main(String[] args) {
             Scanner scanner = new Scanner(System.in);
             Random random = new Random();
@@ -38,6 +38,5 @@ import java.util.Scanner;
             }
         }
     }
-
 
 
